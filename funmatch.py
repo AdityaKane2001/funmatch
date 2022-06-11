2a4014dbbebe1fc86b3a914f33dbc702d6fd3345
Aditya Kane <adityakane1@gmail.com>
2022-06-11 20:14:17 +0000

    ckpt

--- funmatch.py
+++ funmatch.py
@@ -1,7 +1,15 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+import wandb
 
 from data import get_dataloaders
 from utils import set_seed
 from model import ConvNeXt, ViT
+
+teacher = ViT()
+student = ConvNeXt()
+
+loss_fn = nn.KLDivLoss()
+
+NUM_EPOCHS = 1000
